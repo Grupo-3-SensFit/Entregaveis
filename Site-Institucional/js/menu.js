a5dc1196471334270170d2ced202685e58745fb9
deivid0067 <deivid3453@outlook.com>
2024-04-13 21:37:21 -0300

    Tela de Inicio da Landing Page Finalizada

---
+++ Site-Institucional/js/menu.js
@@ -0,0 +1,9 @@
+function menuLateral(e) {
+
+    let navlist = document.querySelector('#navbar-list');
+    let mobileButton = document.querySelector('#mobile-button')
+    mobileButton.classList.toggle('active');
+    navlist.classList.toggle('show')
+
+
+}

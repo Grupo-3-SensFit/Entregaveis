ede1c207184a851cf3bcb49da2dbd8a65c768b14
Isaias-Oliveira <isaiasoliveirabjj@gmail.com>
2024-05-02 14:58:45 -0300

    Mudança no código do arduíno

--- Código ARDUÍNO/sketch_mar21a/sketch_mar21a.ino
+++ Código ARDUÍNO/sketch_mar21a/sketch_mar21a.ino
@@ -1,4 +1,8 @@
 int pinoSensor = 7;
+int fkSensor1 = 1;
+int fkSensor2 = 2;
+int fkSensor3 = 3;
+
 
 void setup() {
   pinMode(pinoSensor, INPUT);
@@ -6,11 +10,23 @@ void setup() {
 }
 
 void loop() {
-  if(digitalRead(pinoSensor) == LOW){
-  Serial.println("1");
-  }
-  else {
-  Serial.println("0");
+  if (digitalRead(pinoSensor) == LOW) {
+    Serial.print("1");
+    Serial.print(";");
+    Serial.println(fkSensor1);
+
+    Serial.print("0");
+    Serial.print(";");
+    Serial.println(fkSensor2);
+
+  } else {
+    Serial.print("0");
+    Serial.print(";");
+    Serial.println(fkSensor1);
+    
+    Serial.print("1");
+    Serial.print(";");
+    Serial.println(fkSensor2);
   }
   delay(1000);
 }
